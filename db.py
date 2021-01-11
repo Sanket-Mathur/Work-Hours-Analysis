9325726e893312a14a0edaa3c08f320590f5d40c
Sanket-Mathur <rajeev.sanket@gmail.com>
2021-01-12 02:13:50 +0530

    Created db.py for management of database

---
+++ db.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+class Management:
+
+    def __init__(self):
+        try:
+            self.db = sqlite3.connect('database.db')
+        except sqlite3.Error as e:
+            print(type(e).__name__)
+
+    def createTable(self):
+        """Creates Table if it doesn't already exist in the database and adds the initial value to the table"""
+        try:
+            self.db.execute('CREATE TABLE IF NOT EXISTS DATA (DATE DATE PRIMARY KEY UNIQUE, WORK NUMERIC, WASTE NUMERIC)')
+            self.db.execute('INSERT INTO DATA VALUES (\'2021-01-01\', 10, 2)')
+            self.db.commit()
+        except sqlite3.Error as e:
+            print(type(e).__name__)
+    
+    def printTable(self):
+        """"Prints all the rows that exist in the table"""
+        try:
+            for row in self.db.execute('SELECT * FROM DATA'):
+                print(row)
+        except sqlite3.Error as e:
+            print(type(e).__name__)
