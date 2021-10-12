4d2d0bb5afb318f3131772ea3bd95226d4841899
Vipul-Bajaj <vipulbjj@gmail.com>
2021-10-13 01:07:51 +0530

    Added time bounds to CLI and GUI app

--- CLI Application/insert.py
+++ CLI Application/insert.py
@@ -6,6 +6,13 @@ from db import Management
 
 database = Management()
 
+
+def within_time_bound(hours: int) -> bool:
+    if hours < 0 or hours > 24:
+        return False
+    return True
+
+
 def update(date, work_hr, waste_hr):
     '''
     Updates a single row corresponds to given date to it.
@@ -32,12 +39,17 @@ def update(date, work_hr, waste_hr):
     if proceed == 'y':
         database.updateData([date, work_hr, waste_hr])
 
+
 def insert():
     try:
         year, month, day = map(int, input('Date (yyyy-mm-dd): ').strip().split('-'))
         date = datetime(year, month, day)
         working = input('Work Hours: ')
         waste = input('Waste Hours: ')
+        if not within_time_bound(int(working)) or not within_time_bound(int(waste)) or not within_time_bound(
+                int(working)+int(waste)):
+            print('Working & Waste Hours should be in the range of 0 to 24 and their sum should not exceed 24 hours.')
+            raise
         date = date.strftime('%Y-%m-%d')
     except:
         print('ERROR: Input Values')
